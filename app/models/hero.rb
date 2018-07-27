c1c396df84208e13037b2629b77eaef2f67b4f0e
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-07-27 18:44:19 +0000

    automatically backed up by learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -2,10 +2,17 @@ class Hero
 
   attr_accessor :name, :power, :bio
 
+  @@all = []
+  
   def initialize(params)
     @name = params[:name]
     @power = params[:power]
     @bio = params[:bio]
+    @@all << self
   end
+
+  def self.all
+    @@all
+  ends
 
 end
